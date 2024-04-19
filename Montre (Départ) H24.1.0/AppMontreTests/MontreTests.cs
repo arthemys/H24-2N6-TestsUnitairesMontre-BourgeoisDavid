f84107fa534d8d12dce42ebe8f37ec44889a06a6
David Bourgeois <davidbourgeois1994@gmail.com>
2024-04-19 10:32:44 -0400

    FCT : Tester l'accesseur Get avec les propriétés initiales

---
+++ Montre (Départ) H24.1.0/AppMontreTests/MontreTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApplicationMontre;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationMontre.Tests
+{
+    [TestClass()]
+    public class MontreTests
+    {
+        #region Tests constructeur initial
+        /// <summary>
+        /// Tester l'accesseur GET avec les propriétés initiales
+        /// </summary>
+        [TestMethod]
+        public void ProprietesGetCasInitRetourInitTest()
+        {
+            // Arranger (Arrange)
+            Montre objMontre = new Montre();
+            int valeurTest = 0;
+            // Agir (Act)
+            // Auditer (Assert)
+            Assert.AreEqual(valeurTest, objMontre.Heures);
+            Assert.AreEqual(valeurTest, objMontre.Minutes);
+            Assert.AreEqual(valeurTest, objMontre.Secondes);
+        }
+
+        #endregion
+    }
+}
